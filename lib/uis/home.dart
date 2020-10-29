298437193aaa70c45eab717d5cf5888fe0f1670f
Chibuike Osita <mozartted@Chibuikes-MacBook-Pro.local>
2020-10-29 19:17:41 +0100

    updated designs for the setup

--- lib/uis/home.dart
+++ lib/uis/home.dart
@@ -11,43 +11,110 @@ class Home extends StatefulWidget {
 }
 
 class _HomePageState extends State<Home> {
-
   Widget titleObject = Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
     children: [
-          // first view
-          Column(
-            children: [
-              SvgPicture.asset('assets/top_header_plus.svg'),
-            ],
-          ),
-          // second view
-          Column(
-            children: [
-              SvgPicture.asset('assets/Instagram_logo_black.svg'),
-            ],
-          ),
-          // theird view
-          Column(
-            children: [
-              SvgPicture.asset('assets/top_header_message.svg'),
-            ],
-          )
+      // first view
+      Column(
+        children: [
+          SvgPicture.asset('assets/top_header_plus.svg'),
+        ],
+      ),
+      // second view
+      Column(
+        children: [
+          SvgPicture.asset('assets/Instagram_logo_black.svg'),
+        ],
+      ),
+      // theird view
+      Column(
+        children: [
+          SvgPicture.asset('assets/top_header_message.svg'),
+        ],
+      )
     ],
   );
 
+  List<Widget> avatarList = List.generate(
+      1200,
+      (i) => Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              CircleAvatar(
+                  radius: 30,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: Image.asset('assets/images/head-view-1.png',
+                        width: 30, height: 30, fit: BoxFit.cover),
+                  )),
+              Text(
+                "Sideone",
+                style: TextStyle(fontWeight: FontWeight.w400, fontSize: 10),
+              )
+            ],
+          )));
+
   @override
   Widget build(BuildContext context) {
+    const background = const Color(0xFFF5F6FA);
     return Scaffold(
+        backgroundColor: background,
         body: Container(
-            padding: EdgeInsets.all(5),
             child: ListView(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.only(top: 50),
-                  child: titleObject,
-                )
-              ],
-            )));
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(5),
+              child: Container(
+                padding: EdgeInsets.only(top: 50),
+                child: titleObject,
+              ),
+            ),
+            Container(
+              height: 100,
+              child: new ListView(
+                  scrollDirection: Axis.horizontal, children: avatarList),
+            ),
+            Container(
+                padding: EdgeInsets.all(30),
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Text('Explore',
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.w700))
+                      ],
+                    ),
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                        padding: EdgeInsets.fromLTRB(10, 0, 3, 0),
+                        decoration: BoxDecoration(
+                          color: Color(0xFFFFFFFF),
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          // border: Border.all(width: 3,color: Colors.green,style: BorderStyle.solid)
+                        ),
+                        child: Row(
+                          children: [
+                            SvgPicture.asset('assets/search.svg'),
+                            Container(
+                              width: 100,
+                              margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                              child: TextField(
+                                cursorWidth: 200,
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ))
+          ],
+        )));
   }
 }
